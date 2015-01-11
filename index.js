39733a9d8f1423a0c0c750dad6313c8c0f3bec37
orweinberger <orweinberger@gmail.com>
2015-01-11 17:07:19 +0200

    Moved to config files, added tests

--- index.js
+++ index.js
@@ -1,84 +1,7 @@
-var bitcoin = require('bitcoin');
-var elasticsearch = require('elasticsearch');
-var async = require('async');
-var es = new elasticsearch.Client({
-  host: 'localhost:9200'
-});
-var client = new bitcoin.Client({
-  host: 'localhost',
-  port: 8332,
-  user: 'YOUR-RPC-USERNAME',
-  pass: 'YOUR-RPC-PASSWORD',
-  timeout: 30000
-});
-function getData(height, cb) {
-  var doc = {};
-  async.waterfall([
-    function (callback) {
-      client.getBlockHash(height, function (err, data) {
-        if (err)
-          callback(err);
-        callback(null, data);
-      });
-    },
-    function (blockhash, callback) {
-      client.getBlock(blockhash, function (err, block) {
-        if (err)
-          return callback(err);
-        doc = block;
-        doc.txinfo = [];
-        callback(null, block);
-      });
-    },
-    function (block, callback) {
-      async.mapSeries(block.tx, function (tx, mapcallback) {
-        client.getRawTransaction(tx, function (err, t) {
-          if (t) {
-            client.decodeRawTransaction(t, function (err, tx) {
-              doc.txinfo.push(tx);
-              mapcallback(err, tx);
-            });
-          }
-          else {
-            mapcallback();
-          }
-        });
-      }, function (err) {
-        callback(err);
-      });
-    }
-  ], function (err) {
-    if (err)
-      return cb(err);
-    cb(null, doc);
-  });
-}
-
-function getLastHeight(cb) {
-  es.indices.exists({
-    index: 'blocks'
-  }, function (err, result) {
-    if (err)
-      return cb(err);
-    if (result === false)
-      return cb(null, 0);
-    if (result === true) {
-      es.search({
-        index: 'blocks',
-        body: {
-          "aggregations": { "max_height": { "max": { "field": "height" }}}
-        }
-      }, function (err, result) {
-        if (err)
-          return cb(err);
-        return cb(null, result.aggregations.max_height.value);
-      });
-    }
-  });
-}
+var common = require('./lib/common');
 
 function run(height) {
-  getData(height, function (err, doc) {
+  common.getData(height, function (err, doc) {
     if (err) {
       console.log('error at:', height);
       return run(height + 1);
@@ -90,7 +13,7 @@ function run(height) {
       "path": "isotime"
     };
     doc.txcount = doc.txinfo.length;
-    es.index({
+    common.es.index({
       index: 'blocks',
       type: 'block',
       id: doc.hash,
@@ -102,7 +25,7 @@ function run(height) {
   });
 }
 
-getLastHeight(function (err, height) {
+common.getLastHeight(function (err, height) {
   if (err)
     throw err;
   run(height);
